9a5daa72aae87ac89746e663edbf2a807b69f4d7
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-01-21 18:07:18 +0530

    update 535.encode-and-decode-tinyurl.java

--- src/535.encode-and-decode-tinyurl.java
+++ src/535.encode-and-decode-tinyurl.java
@@ -50,8 +50,9 @@ public class Codec {
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return shortToLongMap.getOrDefault(shortUrl, "");
+        return shortToLongMap.get(shortUrl);
     }
+
 }
 
 // Your Codec object will be instantiated and called as such:
