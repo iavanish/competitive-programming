f7c946bfaad3edb8f0986ca085f9d08df4734688
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-02-03 20:14:22 +0530

    update 63.unique-paths-ii.java

--- src/63.unique-paths-ii.java
+++ src/63.unique-paths-ii.java
@@ -63,35 +63,26 @@ class Solution {
         if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) {
             return 0;
         }
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(obstacleGrid[i][j] == 1) {
-                    obstacleGrid[i][j] = -1;
-                }
-            }
-        }
         obstacleGrid[0][0] = 1;
 
         for(int i = 0; i < m ; i++) {
             for(int j = 0; j < n; j++) {
-                if((i == 0 && j == 0) || obstacleGrid[i][j] == -1) {
+                if(i == 0 && j == 0) {
                     continue;
                 }
-                if((i == 0 && obstacleGrid[i][j-1] != -1) || (j == 0 && obstacleGrid[i-1][j] != -1)) {
-                    obstacleGrid[i][j] = 1;
+                if(obstacleGrid[i][j] == 1) {
+                    obstacleGrid[i][j] = 0;
                     continue;
                 }
-                if((i == 0 && obstacleGrid[i][j-1] == -1) || (j == 0 && obstacleGrid[i-1][j] == -1)) {
-                    obstacleGrid[i][j] = -1;
+                if(i == 0) {
+                    obstacleGrid[i][j] = obstacleGrid[i][j-1];
                     continue;
                 }
-                if(obstacleGrid[i-1][j] != -1) {
+                if(j == 0) {
                     obstacleGrid[i][j] = obstacleGrid[i-1][j];
+                    continue;
                 }
-                if(obstacleGrid[i][j-1] != -1) {
-                    obstacleGrid[i][j] += obstacleGrid[i][j-1];
-                }
+                obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
             }
         }
 
