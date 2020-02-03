dd1f8daabba0bfadf3743c4df53052876c6299b9
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-02-03 20:07:51 +0530

    update 63.unique-paths-ii.java

--- src/63.unique-paths-ii.java
+++ src/63.unique-paths-ii.java
@@ -63,24 +63,15 @@ class Solution {
         if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) {
             return 0;
         }
-        obstacleGrid[0][0] = 1;
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(i == 0 && j == 0) {
-                    continue;
-                }
                 if(obstacleGrid[i][j] == 1) {
                     obstacleGrid[i][j] = -1;
                 }
-                else if(i == 0 && obstacleGrid[i][j-1] != -1) {
-                    obstacleGrid[i][j] = 1;
-                }
-                else if(j == 0 && obstacleGrid[i-1][j] != -1) {
-                    obstacleGrid[i][j] = 1;
-                }
             }
         }
+        obstacleGrid[0][0] = 1;
 
         for(int i = 0; i < m ; i++) {
             for(int j = 0; j < n; j++) {
