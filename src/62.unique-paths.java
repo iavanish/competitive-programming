613c67a53fc00bb68ffa1d06bce90998beb5a630
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-02-03 19:49:19 +0530

    update 62.unique-paths.java

--- src/62.unique-paths.java
+++ src/62.unique-paths.java
@@ -48,23 +48,22 @@
  * 
  */
 
+import java.util.Arrays;
+
 // @lc code=start
 class Solution {
 
     public int uniquePaths(int m, int n) {
-        int[][] paths = new int[m][n];
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(i == 0 || j == 0) {
-                    paths[i][j] = 1;
-                }
-                else {
-                    paths[i][j] = paths[i-1][j] + paths[i][j-1];
-                }
+        int[] paths = new int[n];
+        Arrays.fill(paths, 1);
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                paths[j] += paths[j-1];
             }
         }
 
-        return paths[m-1][n-1];
+
+        return paths[n-1];
     }
 
 }
