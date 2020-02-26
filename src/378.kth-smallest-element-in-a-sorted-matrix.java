2c877e7049ac65989f2e852d4108824a92a1c2e7
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-02-26 14:47:37 +0530

    update 378.kth-smallest-element-in-a-sorted-matrix.java

---
+++ src/378.kth-smallest-element-in-a-sorted-matrix.java
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=378 lang=java
+ *
+ * [378] Kth Smallest Element in a Sorted Matrix
+ *
+ * https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/description/
+ *
+ * algorithms
+ * Medium (51.68%)
+ * Likes:    1798
+ * Dislikes: 106
+ * Total Accepted:    154.4K
+ * Total Submissions: 295.6K
+ * Testcase Example:  '[[1,5,9],[10,11,13],[12,13,15]]\n8'
+ *
+ * Given a n x n matrix where each of the rows and columns are sorted in
+ * ascending order, find the kth smallest element in the matrix.
+ * 
+ * 
+ * Note that it is the kth smallest element in the sorted order, not the kth
+ * distinct element.
+ * 
+ * 
+ * Example:
+ * 
+ * matrix = [
+ * ⁠  [ 1,  5,  9],
+ * ⁠  [10, 11, 13],
+ * ⁠  [12, 13, 15]
+ * ],
+ * k = 8,
+ * 
+ * return 13.
+ * 
+ * 
+ * 
+ * Note: 
+ * You may assume k is always valid, 1 ≤ k ≤ n^2.
+ */
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+// @lc code=start
+class Solution {
+
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int start = matrix[0][0];
+        int end = matrix[n-1][n-1];
+        while (start < end) {
+            int middle = start + (end - start) / 2;
+            int count = 0;
+            int j = n-1;
+            for (int i = 0; i < n; i++) {
+                while (j >= 0 && matrix[i][j] > middle) {
+                    j--;
+                }
+                count += (j+1);
+            }
+            if (count < k) {
+                start = middle+1;
+            }
+            else {
+                end = middle;
+            }
+        }
+
+        return start;
+    }
+
+}
+// @lc code=end
