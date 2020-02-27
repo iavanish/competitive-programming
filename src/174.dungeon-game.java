a51fa46b6e2f0ca0a1e59576febedd84bcbb43a5
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-02-27 19:53:13 +0530

    update 174.dungeon-game.java

--- src/174.dungeon-game.java
+++ src/174.dungeon-game.java
@@ -91,25 +91,24 @@ class Solution {
         }
         int n = dungeon[0].length;
 
-        int[][] dp = new int[m][n];
         for (int i = m-1; i >= 0; i--) {
             for (int j = n-1; j >= 0; j--) {
                 if (i == m-1 && j == n-1) {
-                    dp[i][j] = Math.max(1, 1 - dungeon[i][j]);
+                    dungeon[i][j] = Math.max(1, 1 - dungeon[i][j]);
                 }
                 else if (i == m-1) {
-                    dp[i][j] = Math.max(1, dp[i][j+1] - dungeon[i][j]);
+                    dungeon[i][j] = Math.max(1, dungeon[i][j+1] - dungeon[i][j]);
                 }
                 else if (j == n-1) {
-                    dp[i][j] = Math.max(1, dp[i+1][j] - dungeon[i][j]);
+                    dungeon[i][j] = Math.max(1, dungeon[i+1][j] - dungeon[i][j]);
                 }
                 else {
-                    dp[i][j] = Math.max(1, Math.min(dp[i+1][j], dp[i][j+1]) - dungeon[i][j]);
+                    dungeon[i][j] = Math.max(1, Math.min(dungeon[i+1][j], dungeon[i][j+1]) - dungeon[i][j]);
                 }
             }
         }
 
-        return dp[0][0];
+        return dungeon[0][0];
     }
 
 }
