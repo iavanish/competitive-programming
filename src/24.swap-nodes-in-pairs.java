334dc51b6acb1062b1593c7e2f695e373ceeb20a
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-03-05 09:30:50 +0000

    update 24.swap-nodes-in-pairs.java

---
+++ src/24.swap-nodes-in-pairs.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=24 lang=java
+ *
+ * [24] Swap Nodes in Pairs
+ *
+ * https://leetcode.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (47.59%)
+ * Likes:    1788
+ * Dislikes: 153
+ * Total Accepted:    414.5K
+ * Total Submissions: 856.9K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * 
+ * You may not modify the values in the list's nodes, only nodes itself may be
+ * changed.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        ListNode nextNext = head.next.next;
+        next.next = head;
+        head.next = swapPairs(nextNext);
+        return next;
+    }
+
+}
+// @lc code=end
