32612152045f26696a66a5b0567e7f050aebfe11
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-03-05 14:06:21 +0000

    update 142.linked-list-cycle-ii.java

---
+++ src/142.linked-list-cycle-ii.java
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode id=142 lang=java
+ *
+ * [142] Linked List Cycle II
+ *
+ * https://leetcode.com/problems/linked-list-cycle-ii/description/
+ *
+ * algorithms
+ * Medium (34.59%)
+ * Likes:    2144
+ * Dislikes: 175
+ * Total Accepted:    279.3K
+ * Total Submissions: 790.2K
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given a linked list, return the node where the cycle begins. If there is no
+ * cycle, return null.
+ * 
+ * To represent a cycle in the given linked list, we use an integer pos which
+ * represents the position (0-indexed) in the linked list where tail connects
+ * to. If pos is -1, then there is no cycle in the linked list.
+ * 
+ * Note: Do not modify the linked list.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: tail connects to node index 1
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2], pos = 0
+ * Output: tail connects to node index 0
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ * 
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = [1], pos = -1
+ * Output: no cycle
+ * Explanation: There is no cycle in the linked list.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Follow-up:
+ * Can you solve it without using extra space?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode turtle = head;
+        ListNode rabbit = head.next;
+        while (rabbit != null && rabbit.next != null && turtle != rabbit) {
+            turtle = turtle.next;
+            rabbit = rabbit.next.next;
+        }
+
+        if (rabbit != turtle) {
+            return null;
+        }
+
+        turtle = turtle.next;
+        rabbit = head;
+        while (turtle != rabbit) {
+            turtle = turtle.next;
+            rabbit = rabbit.next;
+        }
+
+        return turtle;
+    }
+
+}
+// @lc code=end
