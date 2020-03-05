b145c0f6350410018b7a9796cce9ad50d647eb86
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-03-05 13:54:03 +0000

    update 141.linked-list-cycle.java

---
+++ src/141.linked-list-cycle.java
@@ -0,0 +1,101 @@
+/*
+ * @lc app=leetcode id=141 lang=java
+ *
+ * [141] Linked List Cycle
+ *
+ * https://leetcode.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (39.09%)
+ * Likes:    2325
+ * Dislikes: 352
+ * Total Accepted:    547.1K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given a linked list, determine if it has a cycle in it.
+ * 
+ * To represent a cycle in the given linked list, we use an integer pos which
+ * represents the position (0-indexed) in the linked list where tail connects
+ * to. If pos is -1, then there is no cycle in the linked list.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2], pos = 0
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = [1], pos = -1
+ * Output: false
+ * Explanation: There is no cycle in the linked list.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Follow up:
+ * 
+ * Can you solve it using O(1) (i.e. constant) memory?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode turtle = head;
+        ListNode rabbit = head.next;
+        while (rabbit != null && rabbit.next != null) {
+            if (turtle == rabbit) {
+                return true;
+            }
+            turtle = turtle.next;
+            rabbit = rabbit.next.next;
+        }
+        return false;
+    }
+
+}
+// @lc code=end
