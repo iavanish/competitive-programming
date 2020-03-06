6f8491d283dfa376c1b709d5013807246051f244
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-03-06 09:12:34 +0000

    update 167.two-sum-ii-input-array-is-sorted.java

---
+++ src/167.two-sum-ii-input-array-is-sorted.java
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=167 lang=java
+ *
+ * [167] Two Sum II - Input array is sorted
+ *
+ * https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+ *
+ * algorithms
+ * Easy (52.13%)
+ * Likes:    1327
+ * Dislikes: 530
+ * Total Accepted:    350.8K
+ * Total Submissions: 666.1K
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers that is already sorted in ascending order, find
+ * two numbers such that they add up to a specific target number.
+ * 
+ * The function twoSum should return indices of the two numbers such that they
+ * add up to the target, where index1 must be less than index2.
+ * 
+ * Note:
+ * 
+ * 
+ * Your returned answers (both index1 and index2) are not zero-based.
+ * You may assume that each input would have exactly one solution and you may
+ * not use the same element twice.
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: numbers = [2,7,11,15], target = 9
+ * Output: [1,2]
+ * Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+ * 
+ */
+
+// @lc code=start
+class Solution {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length-1;
+        while (true) {
+            int sum = numbers[start] + numbers[end];
+            if (sum == target) {
+                return new int[] {start+1, end+1};
+            }
+            else if(sum < target) {
+                start++;
+            }
+            else {
+                end--;
+            }
+        }
+    }
+
+}
+// @lc code=end
