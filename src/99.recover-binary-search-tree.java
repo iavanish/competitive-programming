1e41a7509527cc11f16192671ce1578f39ae4e45
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-03-11 16:07:08 +0000

    update 99.recover-binary-search-tree.java

---
+++ src/99.recover-binary-search-tree.java
@@ -0,0 +1,124 @@
+/*
+ * @lc app=leetcode id=99 lang=java
+ *
+ * [99] Recover Binary Search Tree
+ *
+ * https://leetcode.com/problems/recover-binary-search-tree/description/
+ *
+ * algorithms
+ * Hard (36.81%)
+ * Likes:    1222
+ * Dislikes: 66
+ * Total Accepted:    146.7K
+ * Total Submissions: 390.2K
+ * Testcase Example:  '[1,3,null,null,2]'
+ *
+ * Two elements of a binary search tree (BST) are swapped by mistake.
+ * 
+ * Recover the tree without changing its structure.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,3,null,null,2]
+ * 
+ * 1
+ * /
+ * 3
+ * \
+ * 2
+ * 
+ * Output: [3,1,null,null,2]
+ * 
+ * 3
+ * /
+ * 1
+ * \
+ * 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [3,1,4,null,null,2]
+ * 
+ * ⁠ 3
+ * ⁠/ \
+ * 1   4
+ * /
+ * 2
+ * 
+ * Output: [2,1,4,null,null,3]
+ * 
+ * ⁠ 2
+ * ⁠/ \
+ * 1   4
+ * /
+ * ⁠ 3
+ * 
+ * 
+ * Follow up:
+ * 
+ * 
+ * A solution using O(n) space is pretty straight forward.
+ * Could you devise a constant space solution?
+ * 
+ * 
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    public void recoverTree(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) < list.get(i-1)) {
+                int j = i;
+                int node = list.get(j--);
+                while (j >= 0 && node < list.get(j)) {
+                    list.set(j+1, list.get(j--));
+                }
+                list.set(j+1, node);
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>(list);
+        populateInorder(root, queue);
+    }
+
+    private void populateInorder(TreeNode root, Queue<Integer> queue) {
+        if (root == null) {
+            return;
+        }
+        populateInorder(root.left, queue);
+        root.val = queue.poll();
+        populateInorder(root.right, queue);
+    }
+
+    private void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+
+}
+// @lc code=end
