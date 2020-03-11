feabfff3fd816ef2d79ad4d87ec740d10374f9dd
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-03-11 07:17:16 +0000

    update 230.kth-smallest-element-in-a-bst.java

---
+++ src/230.kth-smallest-element-in-a-bst.java
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode id=230 lang=java
+ *
+ * [230] Kth Smallest Element in a BST
+ *
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (54.98%)
+ * Likes:    1851
+ * Dislikes: 53
+ * Total Accepted:    309.4K
+ * Total Submissions: 551.4K
+ * Testcase Example:  '[3,1,4,null,2]\n1'
+ *
+ * Given a binary search tree, write a function kthSmallest to find the kth
+ * smallest element in it.
+ * 
+ * Note: 
+ * You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,1,4,null,2], k = 1
+ * ⁠  3
+ * ⁠ / \
+ * ⁠1   4
+ * ⁠ \
+ * 2
+ * Output: 1
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [5,3,6,2,4,null,null,1], k = 3
+ * ⁠      5
+ * ⁠     / \
+ * ⁠    3   6
+ * ⁠   / \
+ * ⁠  2   4
+ * ⁠ /
+ * ⁠1
+ * Output: 3
+ * 
+ * 
+ * Follow up:
+ * What if the BST is modified (insert/delete operations) often and you need to
+ * find the kth smallest frequently? How would you optimize the kthSmallest
+ * routine?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    private int k;
+
+    public int kthSmallest(TreeNode root, int k) {
+        this.k = k;
+        return kthSmallest(root);
+    }
+
+    private int kthSmallest(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+        int left = kthSmallest(root.left);
+        if (left != -1) {
+            return left;
+        }
+        if (k == 1) {
+            return root.val;
+        }
+        k--;
+        return kthSmallest(root.right);
+    }
+
+}
+// @lc code=end
