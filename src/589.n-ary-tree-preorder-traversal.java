52257c0fab5928f328acbf6f4e47fb1da20e6d54
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-04-05 18:13:57 +0000

    update 589.n-ary-tree-preorder-traversal.java

---
+++ src/589.n-ary-tree-preorder-traversal.java
@@ -0,0 +1,103 @@
+/*
+ * @lc app=leetcode id=589 lang=java
+ *
+ * [589] N-ary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (69.69%)
+ * Likes:    457
+ * Dislikes: 53
+ * Total Accepted:    87K
+ * Total Submissions: 123K
+ * Testcase Example:  '[1,null,3,2,4,null,5,6]'
+ *
+ * Given an n-ary tree, return the preorder traversal of its nodes' values.
+ * 
+ * Nary-Tree input serialization is represented in their level order traversal,
+ * each group of children is separated by the null value (See examples).
+ * 
+ * 
+ * 
+ * Follow up:
+ * 
+ * Recursive solution is trivial, could you do it iteratively?
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input: root = [1,null,3,2,4,null,5,6]
+ * Output: [1,3,5,6,2,4]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * 
+ * 
+ * Input: root =
+ * [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+ * Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The height of the n-ary tree is less than or equal to 1000
+ * The total number of nodes is between [0, 10^4]
+ * 
+ * 
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Solution {
+
+    public List<Integer> preorder(Node root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        List<Integer> preorder = new ArrayList<>();
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.empty()) {
+            Node node = stack.pop();
+            preorder.add(node.val);
+            for (int i = node.children.size()-1; i >= 0; i--) {
+                stack.push(node.children.get(i));
+            }
+        }
+
+        return preorder;
+    }
+
+}
+// @lc code=end
