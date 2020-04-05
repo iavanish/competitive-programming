0597f4c6eb3d4a44ada2143f91471f66932c5859
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-04-05 18:40:02 +0000

    update 429.n-ary-tree-level-order-traversal.java

---
+++ src/429.n-ary-tree-level-order-traversal.java
@@ -0,0 +1,109 @@
+/*
+ * @lc app=leetcode id=429 lang=java
+ *
+ * [429] N-ary Tree Level Order Traversal
+ *
+ * https://leetcode.com/problems/n-ary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (61.68%)
+ * Likes:    462
+ * Dislikes: 40
+ * Total Accepted:    63.8K
+ * Total Submissions: 101.3K
+ * Testcase Example:  '[1,null,3,2,4,null,5,6]'
+ *
+ * Given an n-ary tree, return the level order traversal of its nodes' values.
+ * 
+ * Nary-Tree input serialization is represented in their level order traversal,
+ * each group of children is separated by the null value (See examples).
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input: root = [1,null,3,2,4,null,5,6]
+ * Output: [[1],[3,2,4],[5,6]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * 
+ * 
+ * Input: root =
+ * [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+ * Output: [[1],[2,3,4,5],[6,7,8,9,10],[11,12,13],[14]]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The height of the n-ary tree is less than or equal to 1000
+ * The total number of nodes is between [0, 10^4]
+ * 
+ * 
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+
+    public List<List<Integer>> levelOrder(Node root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        List<List<Node>> levelOrderNodes = new ArrayList<>();
+        List<List<Integer>> levelOrderValues = new ArrayList<>();
+        levelOrderNodes.add(Arrays.asList(root));
+        levelOrderValues.add(Arrays.asList(root.val));
+
+        while (true) {
+            List<Node> nodes = new ArrayList<>();
+            List<Integer> values = new ArrayList<>();
+            for (int i = 0; i < levelOrderNodes.get(levelOrderNodes.size()-1).size(); i++) {
+                if (levelOrderNodes.get(levelOrderNodes.size()-1).get(i).children != null) {
+                    for (int j = 0; j < levelOrderNodes.get(levelOrderNodes.size()-1).get(i).children.size(); j++) {
+                        nodes.add(levelOrderNodes.get(levelOrderNodes.size()-1).get(i).children.get(j));
+                        values.add(levelOrderNodes.get(levelOrderNodes.size()-1).get(i).children.get(j).val);
+                    }
+                }
+            }
+            if (nodes.isEmpty()) {
+                break;
+            }
+            levelOrderNodes.add(nodes);
+            levelOrderValues.add(values);
+        }
+
+        return levelOrderValues;
+    }
+
+}
+// @lc code=end
