df99b26f36252b8a4b784b741081b14c3120ba0d
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-04-06 19:01:29 +0000

    update 1019.next-greater-node-in-linked-list.java

---
+++ src/1019.next-greater-node-in-linked-list.java
@@ -0,0 +1,124 @@
+/*
+ * @lc app=leetcode id=1019 lang=java
+ *
+ * [1019] Next Greater Node In Linked List
+ *
+ * https://leetcode.com/problems/next-greater-node-in-linked-list/description/
+ *
+ * algorithms
+ * Medium (56.72%)
+ * Likes:    551
+ * Dislikes: 35
+ * Total Accepted:    33K
+ * Total Submissions: 58.2K
+ * Testcase Example:  '[2,1,5]'
+ *
+ * We are given a linked list with head as the first node.  Let's number the
+ * nodes in the list: node_1, node_2, node_3, ... etc.
+ * 
+ * Each node may have a next larger value: for node_i, next_larger(node_i) is
+ * the node_j.val such that j > i, node_j.val > node_i.val, and j is the
+ * smallest possible choice.  If such a j does not exist, the next larger value
+ * is 0.
+ * 
+ * Return an array of integers answer, where answer[i] =
+ * next_larger(node_{i+1}).
+ * 
+ * Note that in the example inputs (not outputs) below, arrays such as [2,1,5]
+ * represent the serialization of a linked list with a head node value of 2,
+ * second node value of 1, and third node value of 5.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [2,1,5]
+ * Output: [5,5,0]
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [2,7,4,3,5]
+ * Output: [7,0,5,5,0]
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [1,7,5,1,9,2,5,1]
+ * Output: [7,9,9,9,0,5,0,0]
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 1 <= node.val <= 10^9 for each node in the linked list.
+ * The given list has length in the range [0, 10000].
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+
+import java.util.Stack;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    public int[] nextLargerNodes(ListNode head) {
+        if (head == null) {
+            return new int[0];
+        }
+
+        int n = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            n++;
+            temp = temp.next;
+        }
+
+        int[] nextLargerNodes = new int[n];
+        temp = head;
+        for (int i = 0; temp != null; i++) {
+            nextLargerNodes[i] = temp.val;
+            temp = temp.next;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(nextLargerNodes[n-1]);
+        nextLargerNodes[n-1] = 0;
+        for (int i = n-2; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nextLargerNodes[i]) {
+                stack.pop();
+            }
+            int tempNextLargerNodes = nextLargerNodes[i];
+            if (!stack.isEmpty()) {
+                nextLargerNodes[i] = stack.peek();
+            }
+            else {
+                nextLargerNodes[i] = 0;
+            }
+            stack.push(tempNextLargerNodes);
+        }
+
+        return nextLargerNodes;
+    }
+
+}
+// @lc code=end
