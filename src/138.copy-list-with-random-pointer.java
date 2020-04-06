29aa986fbe751478374363a08bf61b0955cb0bca
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-04-06 21:49:08 +0000

    update 138.copy-list-with-random-pointer.java

---
+++ src/138.copy-list-with-random-pointer.java
@@ -0,0 +1,122 @@
+/*
+ * @lc app=leetcode id=138 lang=java
+ *
+ * [138] Copy List with Random Pointer
+ *
+ * https://leetcode.com/problems/copy-list-with-random-pointer/description/
+ *
+ * algorithms
+ * Medium (31.28%)
+ * Likes:    2698
+ * Dislikes: 599
+ * Total Accepted:    365.4K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[[7,null],[13,0],[11,4],[10,2],[1,0]]'
+ *
+ * A linked list is given such that each node contains an additional random
+ * pointer which could point to any node in the list or null.
+ * 
+ * Return a deep copy of the list.
+ * 
+ * The Linked List is represented in the input/output as a list of n nodes.
+ * Each node is represented as a pair of [val, random_index] where:
+ * 
+ * 
+ * val: an integer representing Node.val
+ * random_index: the index of the node (range from 0 to n-1) where random
+ * pointer points to, or null if it does not point to any node.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [[1,1],[2,1]]
+ * Output: [[1,1],[2,1]]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * 
+ * 
+ * Input: head = [[3,null],[3,0],[3,null]]
+ * Output: [[3,null],[3,0],[3,null]]
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: head = []
+ * Output: []
+ * Explanation: Given linked list is empty (null pointer), so return null.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * -10000 <= Node.val <= 10000
+ * Node.random is null or pointing to a node in the linked list.
+ * Number of Nodes will not exceed 1000.
+ * 
+ * 
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+import java.util.HashMap;
+
+class Solution {
+
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        Map<Node, Node> map = new HashMap<>();
+        Node copy = copyRandomList(head, map);
+        Node temp = head;
+        while (temp != null) {
+            if (temp.random != null) {
+                map.get(temp).random = map.get(temp.random);
+            }
+            temp = temp.next;
+        }
+
+        return copy;
+    }
+
+    private Node copyRandomList(Node head, Map<Node, Node> map) {
+        if (head == null) {
+            return null;
+        }
+        Node temp = new Node(head.val);
+        map.put(head, temp);
+        temp.next = copyRandomList(head.next, map);
+        return temp;
+    }
+
+}
+// @lc code=end
