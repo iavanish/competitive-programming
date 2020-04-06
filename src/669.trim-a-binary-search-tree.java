9709fcc1354c6448464de2a0577294a5f73d228e
Avanish Singh <32356467+iavanish@users.noreply.github.com>
2020-04-06 14:08:17 +0000

    update 669.trim-a-binary-search-tree.java

---
+++ src/669.trim-a-binary-search-tree.java
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode id=669 lang=java
+ *
+ * [669] Trim a Binary Search Tree
+ *
+ * https://leetcode.com/problems/trim-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (61.51%)
+ * Likes:    1537
+ * Dislikes: 155
+ * Total Accepted:    90.9K
+ * Total Submissions: 146.3K
+ * Testcase Example:  '[1,0,2]\n1\n2'
+ *
+ * 
+ * Given a binary search tree and the lowest and highest boundaries as L and R,
+ * trim the tree so that all its elements lies in [L, R] (R >= L). You might
+ * need to change the root of the tree, so the result should return the new
+ * root of the trimmed binary search tree.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 0   2
+ * 
+ * ⁠ L = 1
+ * ⁠ R = 2
+ * 
+ * Output: 
+ * ⁠   1
+ * ⁠     \
+ * ⁠      2
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 0   4
+ * ⁠  \
+ * ⁠   2
+ * ⁠  /
+ * ⁠ 1
+ * 
+ * ⁠ L = 1
+ * ⁠ R = 3
+ * 
+ * Output: 
+ * ⁠     3
+ * ⁠    / 
+ * ⁠  2   
+ * ⁠ /
+ * ⁠1
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return null;
+        }
+        if (R < root.val) {
+            return trimBST(root.left, L, R);
+        }
+        if (root.val < L) {
+            return trimBST(root.right, L, R);
+        }
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+        return root;
+    }
+
+}
+// @lc code=end
